967135d8e957bed315629c87efe1b6ec2d6ad005
RichardTao3014 <xtao10@ucsc.edu>
2025-01-20 17:56:08 -0800

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-//console.log("Hello World!")
 let config = {
     type: Phaser.AUTO,
     width: 640,
-    height: 480
-  }
+    height: 480,
+    scene: [ Menu, Play ],
+}
 
 let game = new Phaser.Game(config)
+
+        // set UI sizes
+        let borderUISize = game.config.height / 15
+        let borderPadding = borderUISize / 3
+
+        // reserve keyboard bindings
+        let keyF, keyR, keyLEFT, keyRIGHT
